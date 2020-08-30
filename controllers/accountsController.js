1ff2ff139a4fcdbf3df585ff2db59fa558535c5d
Lucas <martinslf@taginfraestrutura.com.br>
2020-08-29 21:49:16 -0300

    finished implementation

--- controllers/accountsController.js
+++ controllers/accountsController.js
@@ -13,9 +13,9 @@ const getAll = async (_, response) => {
 
 const getByAgenciaAndConta = async (request, response) => {
     try {
-        const { agencia, conta } = request.params;
+        const { agencia, conta } = request.query;
 
-        if (!conta || !agencia || !value > 0) {
+        if (!conta || !agencia) {
             response.status(400).send("params invalids");
             return;
         }
@@ -28,6 +28,69 @@ const getByAgenciaAndConta = async (request, response) => {
         }
 
         response.status(200).send(account);
+    } catch (error) {
+        response.status(500).send(error.message);
+    }
+}
+
+const getAvarageByAgencia = async (request, response) => {
+    try {
+        const { agencia } = request.query;
+
+        if (!agencia) {
+            response.status(400).send("params invalids");
+            return;
+        }
+
+        const accounts = await Account.find({ agencia: agencia });
+
+        if (!accounts) {
+            response.status(400).send("agencia not found");
+            return;
+        }
+
+        const totalValues = accounts.reduce((sum, account) => {
+            return sum + account.balance;
+        }, 0);
+
+        const avarage = totalValues / accounts.length;
+
+        response.status(200).send({ agencia: agencia, average: avarage });
+
+    } catch (error) {
+        response.status(500).send(error.message);
+    }
+}
+
+const getListMinValues = async (request, response) => {
+    try {
+        const { limit } = request.query;
+
+        if (Number(limit) <= 0) {
+            response.status(400).send("params invalids");
+            return;
+        }
+
+        const data = await Account.find({}, [], { limit: Number(limit), sort: { balance: 1 } });
+
+        response.status(200).send(data);
+    } catch (error) {
+        response.status(500).send(error.message);
+    }
+}
+
+const getListMaxValues = async (request, response) => {
+    try {
+        const { limit } = request.query;
+
+        if (Number(limit) <= 0) {
+            response.status(400).send("params invalids");
+            return;
+        }
+
+        const data = await Account.find({}, [], { limit: Number(limit), sort: { balance: -1, name: 1 } });
+
+        response.status(200).send(data);
     } catch (error) {
         response.status(500).send(error.message);
     }
@@ -104,4 +167,131 @@ const debit = async (request, response) => {
     }
 }
 
-export { getAll, credit, debit, getByAgenciaAndConta };
+const transferValue = async (request, response) => {
+    try {
+        const { contaOrigin, contaDestiny, value } = request.body;
+
+        if (!contaOrigin || !contaDestiny || !value > 0) {
+            response.status(400).send("params invalids");
+            return;
+        }
+        var accountOrigin = await Account.findOne({ conta: contaOrigin });
+
+        if (!accountOrigin) {
+            response.status(400).send("account debtor invalid");
+            return;
+        }
+
+        var accountDestiny = await Account.findOne({ conta: contaDestiny });
+
+        if (!accountDestiny) {
+            response.status(400).send("account creditor invalid");
+            return;
+        }
+
+        if (accountOrigin.agencia === accountDestiny.agencia) {
+            if (!(accountOrigin.balance >= value)) {
+                response.status(400).send("not enough balance ");
+                return;
+            }
+
+            accountOrigin.balance -= value;
+            accountDestiny.balance += value;
+
+            const origin = await Account.findByIdAndUpdate({ _id: accountOrigin._id }, accountOrigin, {
+                new: true,
+            });
+
+            if (!origin) {
+                response.status(400).send("Fail to remove credit");
+                return;
+            }
+
+            const destiny = await Account.findByIdAndUpdate({ _id: accountDestiny._id }, accountDestiny, {
+                new: true,
+            });
+
+            if (!destiny) {
+                response.status(400).send("Fail to transfer credit");
+                return;
+            }
+
+            response.status(200).send(origin);
+        } else {
+            if (!(accountOrigin.balance >= (value + 8))) {
+                response.status(400).send("not enough balance ");
+                return;
+            }
+
+            accountOrigin.balance -= (value + 8);
+            accountDestiny.balance += value;
+
+            const origin = await Account.findByIdAndUpdate({ _id: accountOrigin._id }, accountOrigin, {
+                new: true,
+            });
+
+            if (!origin) {
+                response.status(400).send("Fail to remove credit");
+                return;
+            }
+
+            const destiny = await Account.findByIdAndUpdate({ _id: accountDestiny._id }, accountDestiny, {
+                new: true,
+            });
+
+            if (!destiny) {
+                response.status(400).send("Fail to transfer credit");
+                return;
+            }
+
+            response.status(200).send(origin);
+        }
+    } catch (error) {
+        response.status(500).send(error.message);
+    }
+}
+
+const setListVip = async (request, response) => {
+    try {
+
+        const agencias = await Account.distinct("agencia");
+
+        for (let index = 0; index < agencias.length; index++) {
+            await Account.findOneAndUpdate({ agencia: agencias[index] }, { agencia: 99 }, { sort: { balance: -1 } });
+        }
+
+        const vipAccounts = await Account.find({ agencia: 99 });
+
+        response.status(200).send(vipAccounts);
+
+    } catch (error) {
+        response.status(500).send(error.message);
+    }
+}
+
+const removeAccount = async (request, response) => {
+    try {
+        const { agencia, conta } = request.query;
+
+        if (!conta || !agencia) {
+            response.status(400).send("params invalids");
+            return;
+        }
+
+        const res = await Account.deleteOne({ conta: conta, agencia: agencia });
+
+        if (res.deletedCount > 0) {
+            const accounts = await Account.countDocuments({ agencia: agencia });
+
+            response.status(200).send({ totalAccounts: accounts });
+
+        } else {
+            response.status(400).send("fail to delete account");
+        }
+
+    } catch (error) {
+        response.status(500).send(error.message);
+    }
+}
+
+export { getAll, credit, debit, getByAgenciaAndConta, removeAccount, transferValue, getAvarageByAgencia, getListMinValues, getListMaxValues, setListVip };
--- routes/accountsRouter.js
+++ routes/accountsRouter.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { getAll, credit, debit, getByAgenciaAndConta } from "../controllers/accountsController.js";
+import { getAll, credit, debit, getByAgenciaAndConta, removeAccount, transferValue, getAvarageByAgencia, getListMinValues, getListMaxValues, setListVip } from "../controllers/accountsController.js";
 
 const router = express.Router();
 
 router.get("/", getAll);
 router.get("/params", getByAgenciaAndConta);
+router.get("/average", getAvarageByAgencia);
+router.get("/minValues", getListMinValues);
+router.get("/maxValues", getListMaxValues);
 router.post("/credit/", credit);
 router.post("/debit/", debit);
+router.post("/transfer/", transferValue);
+router.post("/lisVip/", setListVip);
+router.delete("/", removeAccount);
 
 export default router;
